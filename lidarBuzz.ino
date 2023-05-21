b104db325139e2c108e60b2cd6b18e0346e7925b
Arnav Rastogi <arnrastogi@ucdavis.edu>
2023-05-21 01:34:34 -0700

    Update and rename arduino_lidar_buzzer.ino to lidarBuzz.ino

--- lidarBuzz.ino
+++ lidarBuzz.ino
@@ -5,20 +5,24 @@
 TFLI2C tflI2C;
 
 int16_t tfDist;                  // distance in centimeters
-int16_t tfAddr = TFL_DEF_ADR;    // Use this default I2C address
+int16_t tfAddr = TFL_DEF_ADR;    // default I2C address
 
+// setting up the serial port, library, and vibration motor
 void setup() {
   pinMode(8, OUTPUT);
   Serial.begin(9600);           // Initialize serial port
   Wire.begin();                   // Initialize Wire library
 }
 
+// main event loop to constantly fetch LiDAR sensor readings and turn on the vibration motor accordingly
 void loop() {
   if (tflI2C.getData(tfDist, tfAddr)) {
     Serial.print(tfDist);         // Print the distance value
     Serial.println(" cm");
-    if (tfDist >= 50) {
+    if (tfDist <= 150) {
+      int delayTime = map(tfDist, 0, 150, 10, 1000);  // Map distance to delay time (adjust the range as needed)
       digitalWrite(8, HIGH);
+      delay(50);
     }
     else {
       digitalWrite(8, LOW);
